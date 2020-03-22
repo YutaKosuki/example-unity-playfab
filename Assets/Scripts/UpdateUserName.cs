46e56f56fa4c837d635ea7bdca264916915a9901
YutaKosuki <bambiyuta@gmail.com>
2020-03-22 17:16:51 +0900

    add UpdateUserName script

---
+++ Assets/Scripts/UpdateUserName.cs
@@ -0,0 +1,33 @@
+﻿using PlayFab;
+using PlayFab.ClientModels;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateUserName : MonoBehaviour
+{
+      [SerializeField] private Text _nameText = default;
+
+    public void UpdateUserDisplayName() {
+        //ユーザ名を指定して、UpdateUserTitleDisplayNameRequestのインスタンスを生成
+        var request = new UpdateUserTitleDisplayNameRequest{
+            DisplayName = _nameText.text
+        };
+
+        //ユーザ名の更新
+        Debug.Log($"ユーザ名の更新開始");
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnUpdateUserNameSuccess, OnUpdateUserNameFailure);
+    }
+
+    //ユーザ名の更新成功
+    private void OnUpdateUserNameSuccess(UpdateUserTitleDisplayNameResult result) {
+        //result.DisplayNameに更新した後のユーザ名が入ってる
+        Debug.Log($"ユーザ名の更新が成功しました : {result.DisplayName}");
+    }
+
+    //ユーザ名の更新失敗
+    private void OnUpdateUserNameFailure(PlayFabError error) {
+        Debug.LogError($"ユーザ名の更新に失敗しました\n{error.GenerateErrorReport()}");
+    }
+}
